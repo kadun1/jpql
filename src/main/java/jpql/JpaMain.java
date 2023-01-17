571efadd950c87da7b80a2f0e6269c1fabcf93c0
kadun1 <kadunjj@gmail.com>
2023-01-18 00:11:26 +0900

    JPQL nullif : 두 값이 같으면 null 반환, 다르면 첫번째 값 반환

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername(null);
+            member.setUsername("관리자");
             member.setAge(10);
             member.setTeam(team);
             member.setMemberType(MemberType.ADMIN);
@@ -30,7 +30,7 @@ public class JpaMain {
             em.clear();
 
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            String query = "select nullif(m.username, '관리자') from Member m";
             List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
 
